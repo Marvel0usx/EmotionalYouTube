c35217e10952bf0a44f372f29191bcb2e068db9d
Harry Wang <marvel0usx@outlook.com>
2020-05-20 18:17:59 -0400

    [debug] fixed null value references

--- emotional-youtube-launcher/popup.js
+++ emotional-youtube-launcher/popup.js
@@ -1,35 +1,19 @@
 // function to trigger analysis
 function Analyze() {
-    var vid = document.getElementById('vid-box').value;
+    var vid = document.getElementById('idbox').value;
     document.getElementById('report').innerHTML = vid;
 }
 
 // function to retrieve vid from storage
-// function setVid() {
-//     vid = "";
-//     chrome.storage.sync.get(['vid'], function (result) {
-//         if (result.vid !== null) {
-//             vid = result.vid;
-//         } else {
-//             return;
-//         }
-//     });
-//     document.getElementById('idbox').value = vid;
-//     chrome.storage.sync.set({'vid': null});
-// }
-
 function setVid() {
-    alert("hello!");
+    // vid = "";
+    // chrome.storage.sync.get(['vid'], function (result) {
+    //     if (result.vid !== null) {
+    //         vid = result.vid;
+    //     } else {
+    //         return;
+    //     }
+    // });
+    // document.getElementById('idbox').value = vid;
+    // chrome.storage.sync.set({'vid': null});
 }
-
-
-// logic without loaded DOM
-if (document.readyState === 'loading') {  // Loading hasn't finished yet
-    document.addEventListener('DOMContentLoaded', setVid(), false);
-    document.getElementById("btn-analyze").addEventListener("click", Analyze());
-} else {                                  // 'DOMContentLoaded' has already fired
-    setVid();
-    document.getElementById("btn-analyze").addEventListener("click", Analyze());
-};
-
-// document.getElementById("btn-analyze").addEventListener("click", Analyze());
