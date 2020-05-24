045cdfadcf38a205d8254984c8614862daf4d81b
Harry Wang <marvel0usx@outlook.com>
2020-05-24 01:42:21 -0400

    [dev] another way out

--- emotional-youtube-launcher/js/content.js
+++ emotional-youtube-launcher/js/content.js
@@ -2,15 +2,45 @@
 function analyze(vid) {
     alert("analysis begins!");
     // TODO change mocking report
-    return "report for" + vid;
+    var report = "Report for "
+    return report.concat(vid);
 }
 
-// listen to click on button
+function showReport(report) {
+    // TODO change mocking showing
+    document.getElementById('report').innerHTML = report.toString();
+}
+
+// parse video id from url
+function parseVid(url) {
+    // use positive lookahead, starting to match after '?v='
+    const regex = /(?<=\?v\=)([A-Za-z0-9_-]+)/;
+    const found = url.match(regex);
+
+    if (found !== null) {
+        return found[0];
+    } else {
+        return null;
+    }
+}
+
+// logic runs after DOM is loaded
 document.addEventListener("DOMContentLoaded", 
     function() {
+        var vid;
+        // find the tab that the extension is evoked on
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, 
+            function(tabs) {
+                if (tabs !== null) {
+                    vid = parseVid(tabs[0].url);
+                    document.getElementById("idbox").value = vid;
+                }
+            }
+        );
+        // listen to button clicks
         document.getElementById("btn-analyze").addEventListener("click", 
             function() {
-                const report = analyze(document.getElementById("idbox"));
+                var report = analyze(document.getElementById("idbox"));
                 if (report) {
                     showReport(report);
                 }
@@ -18,18 +48,3 @@ document.addEventListener("DOMContentLoaded",
         );
     }, false
 );
-
-// listen to message from background, and set vid input box
-chrome.runtime.onMessage.addListener(
-    function (request) {
-        if (request.vid) {
-            document.getElementById('idbox').value = request.vid;
-        }
-    }
-);
-
-function showReport(report) {
-    alert("showing result!");
-    // TODO change mocking showing
-    document.getElementById('report').innerHTML = report;
-}
